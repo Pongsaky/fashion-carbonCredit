ee1bec01cef7195a30e210380c667a1b2f6c028b
Pongsaky <pongsakon.kawe@gmail.com>
2023-08-02 11:36:25 +0700

    Add overlay upload on new profile page

--- static/js/profile.js
+++ static/js/profile.js
@@ -19,3 +19,25 @@ select_lists.forEach((select_list, index) => {
         }
     })
 })
+
+// Click overlay of upload text
+// JavaScript function to trigger the file input on "Upload Image" click
+document.getElementById("upload-text").addEventListener("click", function () {
+    // console.log("Clicked")
+    document.getElementById("profile-img-upload").click();
+});
+
+// JavaScript function to update the profile image on file selection
+document.getElementById("profile-img-upload").addEventListener("change", function () {
+    let fileInput = document.getElementById("profile-img-upload");
+    let file = fileInput.files[0];
+
+    if (file) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            // Display the selected image as the profile image
+            document.getElementById("profile-image").src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+});
